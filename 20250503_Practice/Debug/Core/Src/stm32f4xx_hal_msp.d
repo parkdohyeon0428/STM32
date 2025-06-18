5749dc36e789214d30d82b6980bacded5db4c5b7
dohyeon <pkppfh@naver.com>
2025-06-18 17:27:02 +0900

    first commit

---
+++ 20250503_Practice/Debug/Core/Src/stm32f4xx_hal_msp.d
@@ -0,0 +1,5 @@
+Core/Src/stm32f4xx_hal_msp.o: ../Core/Src/stm32f4xx_hal_msp.c \
+ ../Core/Inc/main.h ../Core/Inc/main.h ../Core/Inc/OV7670.h
+../Core/Inc/main.h:
+../Core/Inc/main.h:
+../Core/Inc/OV7670.h:
---
+++ 250618_GPIO_Baremetal/Debug/Src/driver/Led_Bar/LED_Bar.d
@@ -0,0 +1,7 @@
+Src/driver/Led_Bar/LED_Bar.o: ../Src/driver/Led_Bar/LED_Bar.c \
+ ../Src/driver/Led_Bar/LED_Bar.h \
+ D:/STM32/250618_GPIO_Baremetal/Src/driver/GPIO/GPIO.h \
+ ../Inc/stm32f411xe.h
+../Src/driver/Led_Bar/LED_Bar.h:
+D:/STM32/250618_GPIO_Baremetal/Src/driver/GPIO/GPIO.h:
+../Inc/stm32f411xe.h:
