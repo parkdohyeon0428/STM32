714e2154a68ffac81b4e7e14c098712db549f7e5
dohyeon <pkppfh@naver.com>
2025-07-02 17:08:50 +0900

    0701

---
+++ 250702_TimeWatch_StopWatch_RTOS3/Core/ap/src/ap_main.c
@@ -0,0 +1,35 @@
+
+#include "ap_main.h"
+
+//void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+//{
+//
+//  if(htim->Instance == TIM2){
+//	  FND_DispDataCallBack();
+//
+//	  TimeWatch_IncTimeCallBack();
+//	  StopWatch_IncTimeCallBack();
+//  }
+//}
+
+int ap_main()
+{
+
+	//HAL_TIM_Base_Start_IT(&htim2);
+	while(1)
+	{
+//		Listener_Excute();
+//		Controller_Excute();
+//		Presenter_Excute();
+	}
+
+	return 0;
+}
+
+
+void ap_init()
+{
+	Listener_Init();
+	Presenter_Init();
+}
+
